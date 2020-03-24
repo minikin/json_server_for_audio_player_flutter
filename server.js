70082f0a57e0553f8c69f245ab22c5d9f8e9e586
minikin <djminikin@gmail.com>
2020-03-24 17:02:31 +0100

    Update server

--- server.js
+++ server.js
@@ -8,22 +8,10 @@ const port = process.env.PORT || 4000;
 server.db = router.db
 server.use(router)
 
-server.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 server.listen(port, () => {
   console.log('JSON Server is running')
 })
 
-
 setInterval(function () {
   https.get('https://api-flutter-audio-player.herokuapp.com');
 }, 300000);
-
-
-// setInterval(function () {
-//   https.get('http://localhost');
-// }, 300000);
