62173dad89b678b3075e8e2f78170443b22c8b05
minikin <djminikin@gmail.com>
2020-03-24 16:10:04 +0100

    Update server

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
 const http = require("http");
+const https = require("https");
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
 
 const port = process.env.PORT || 4000;
 
-server.use(middlewares)
+server.db = router.db
+server.use(auth)
 server.use(router)
-
-server.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 server.listen(port, () => {
   console.log('JSON Server is running')
@@ -21,5 +17,5 @@ server.listen(port, () => {
 
 
 setInterval(function () {
-  http.get('https://api-flutter-audio-player.herokuapp.com');
+  https.get('https://api-flutter-audio-player.herokuapp.com');
 }, 300000);
