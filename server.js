d49f825ae2a43a8ba86c55147f8575cf167b8d43
minikin <djminikin@gmail.com>
2020-03-25 09:32:39 +0100

    Update server, LICENSE

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+const path = require('path')
+const express = require('express')
 const jsonServer = require('json-server')
-const https = require("https");
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const data = require('./db.json')
 
 const port = process.env.PORT || 4000;
+const router = jsonServer.router(data)
+const server = jsonServer.create()
 
-server.db = router.db
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+server.use(jsonServer.defaults(['./public']))
 server.use(router)
+server.listen(port)
 
-server.listen(port, () => {
-  console.log('JSON Server is running')
-})
+console.log('Server is running on:', 'http://localhost:' + port);
 
 setInterval(function () {
   https.get('https://api-flutter-audio-player.herokuapp.com');
